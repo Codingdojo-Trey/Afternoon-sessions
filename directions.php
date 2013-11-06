c9d34f288f09ddcf09ba3f8242e5ff61fb91e82a
Trey Villafane <tvillafane1990@gmail.com>
2013-11-05 17:21:39 -0800

    cleaned up the code

--- directions.php
+++ directions.php
@@ -4,27 +4,23 @@
 	<script type="text/javascript" src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
 	<script type="text/javascript">
 		$(document).ready(function(){
-			function callback(word)
-			{
-				alert('are you crazy?  You want to go to '+word)
-			};
 			$('form').submit(function(){
 				var from = $('#origin').val();
 				var to = $('#destination').val();
-				var url = 'http://maps.googleapis.com/maps/api/directions/json?origin='+from+'&destination='+to+'&sensor=false';
-				$.post(url, function(data){
-					console.log(url);
+				var url = 'http://maps.googleapis.com/maps/api/directions/json?&sensor=false';
+				console.log('Form Info', $(this).serialize())
+				$.get($(this).attr('action'), $(this).serialize(), function(data){
 					end = data.routes[0].legs[0].end_address;
-					callback(end);
-					$('ol').html(' ');
-					$('#start').text(data.routes[0].legs[0].start_address);
-					$('#end').text(data.routes[0].legs[0].end_address);
+					$('#start').text(data.routes[0].legs[0].start_address); //adding text
+					$('#end').text(data.routes[0].legs[0].end_address); //adding text
 					count = data.routes[0].legs[0].steps.length   //this calculates how many different steps there are in this route
 					step = data.routes[0].legs[0].steps  //this is the part of the returned JSON that lists each step of the navigation, complete with some HTML markup.
+					var list_of_directions = "" ; 
 					for(x = 0; x < count ; x++)
 					{
-						$('ol').append("<li>"+step[x].html_instructions+"</li>");
+						list_of_directions += "<li>"+step[x].html_instructions+"</li>";
 					};
+					$('ol').append(list_of_directions);
 				}, 'json');
 				return false;
 			});
@@ -32,9 +28,10 @@
 	</script>
 </head>
 <body>
-	<form>
+	<form action='http://maps.googleapis.com/maps/api/directions/json?' method='get'>  <!--For this API, we need to do a get INSTEAD of  POST -->
 		From: <input type = 'text' name = 'origin' id = 'origin'>
 		To: <input type = 'text' name = 'destination' id = 'destination'>
+		<input type='hidden' name='sensor' value='false'>
 		<input type = 'submit' value = 'get directions!'>
 	</form>	
 	<div id = 'results'>
